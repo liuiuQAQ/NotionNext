05c31609357b379f766f0ef9f437761c04773d84
liuiuQAQ <103240867+liuiuQAQ@users.noreply.github.com>
2026-07-13 19:18:32 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,16 @@
 import BLOG from '@/blog.config'
 import { getAllPosts } from '@/lib/db/SiteDataApi'
-import Layout from '@/layouts/layout'
-import Hero from '@/themes/proxio/components/Hero'
-import PostGrid from '@/themes/proxio/components/PostGrid'
 
 export async function getStaticProps() {
   const allPosts = await getAllPosts()
   return {
-    props: { allPosts },
+    props: {
+      allPosts
+    },
     revalidate: 60
   }
 }
 
 export default function Home({ allPosts }) {
-  return (
-    <Layout>
-      <Hero />
-      <PostGrid posts={allPosts} />
-    </Layout>
-  )
+  return null
 }
