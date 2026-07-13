8aace02d4dd49fb642d8cc2d77feee60aea1bc5a
liuiuQAQ <103240867+liuiuQAQ@users.noreply.github.com>
2026-07-13 19:09:02 +0800

    Update index.js

--- themes/heo/index.js
+++ themes/heo/index.js
@@ -1,29 +1,26 @@
 // themes/heo/index.js
 
-import { useGlobal } from '@lib/global'
 import RecentUpdateGrid from './components/RecentUpdateGrid'
 import AllCharGrid from './components/AllCharGrid'
 
 export default function ThemeLayout({ children, posts, ...props }) {
-  const { themeConfig } = useGlobal()
-  
-  // 从配置中读取开关
-  const showSearch = themeConfig?.HERO_LAYOUT?.showSearch !== false
-  const showRecentUpdate = themeConfig?.HERO_LAYOUT?.showRecentUpdate !== false
-  const showAllChars = themeConfig?.HERO_LAYOUT?.showAllChars !== false
-  
   return (
     <div className="min-h-screen bg-white">
-      {showSearch && (
-        <div className="py-10 flex justify-center">
-          <input 
-            placeholder="搜索角色..." 
-            className="border-2 border-black w-[600px] sm:w-[90%] px-3 py-2 text-lg rounded-lg focus:outline-none focus:border-orange-500"
-          />
-        </div>
-      )}
-      {showRecentUpdate && <RecentUpdateGrid posts={posts} />}
-      {showAllChars && <AllCharGrid posts={posts} />}
+      {/* 顶部搜索栏 */}
+      <div className="py-10 flex justify-center">
+        <input
+          placeholder="搜索角色..."
+          className="border-2 border-black w-[600px] sm:w-[90%] px-3 py-2 text-lg rounded-lg focus:outline-none focus:border-orange-500"
+        />
+      </div>
+
+      {/* 最近更新 */}
+      <RecentUpdateGrid posts={posts} />
+
+      {/* 全部角色 */}
+      <AllCharGrid posts={posts} />
+
+      {/* 子内容 */}
       {children}
     </div>
   )
