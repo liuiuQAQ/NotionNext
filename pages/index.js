1c0454e5c1903cea8b5a96b7e4b052b07980b6e0
liuiuQAQ <103240867+liuiuQAQ@users.noreply.github.com>
2026-07-13 18:47:52 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import RecentUpdateGrid from '@themes/heo/components/RecentUpdateGrid'
 import AllCharGrid from '@themes/heo/components/AllCharGrid'
 import { getAllPosts } from '@lib/db/SiteDataApi'
 
-// ⚠️ 这个函数必须有！负责从 Notion 获取数据
+// 这个函数必须有！负责从 Notion 获取数据
 export async function getStaticProps() {
   let allPosts = []
   try {
@@ -17,7 +17,7 @@ export async function getStaticProps() {
     props: {
       allPosts
     },
-    revalidate: 60  // 60秒后重新生成
+    revalidate: 60 // 60秒后重新生成
   }
 }
 
@@ -26,9 +26,9 @@ export default function Home({ allPosts }) {
     <main className="min-h-screen bg-white">
       {/* 顶部居中搜索框 */}
       <div className="py-10 flex justify-center">
-        <input 
-          placeholder="搜索" 
-          className="border-2 border-black w-[600px] sm:w-[90%] px-3 py-2 text-lg rounded-lg focus:outline-none focus:border-orange-500" 
+        <input
+          placeholder="搜索"
+          className="border-2 border-black w-[600px] sm:w-[90%] px-3 py-2 text-lg rounded-lg focus:outline-none focus:border-orange-500"
         />
       </div>
 
